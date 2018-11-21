3e963f4439cb8587dfb48e98260726288cc6813c
kwantaing <45217862+kwantaing@users.noreply.github.com>
2018-11-20 17:25:58 -0800

    Add files via upload

---
+++ Phase 5/declaration.js
@@ -0,0 +1,23 @@
+var images = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg",
+              "a.jpg","b.jpg","c.jpg","d.jpg","e.jpg","f.jpg"];
+var captions = ["Apple","Microsoft","Hewlett Packard","Alienware","Dell","Linux",
+                "iPhoneXR","iPhone XS Max","iPhone XS","iPhoneX","iPhone 8 Plus","iPhone 8"];
+var descriptions = ["Apple is a company based in Cupertino, California. Well known for its iPhone product range. Also produces computers and that enrich our society today.",
+                    "Microsoft is a company that was founded by Bill Gates. The company revolutionized Microsoft Windows OS. Without Microsoft, mobile computing would not the same as today.",
+                    "HP is a company that specializes in a variety of computer hardware products. They are well known in the business industry for their monitors. In addition, they are used by professional networking-industries.",
+                    "Alienware is a company that is well known for their gaming computers. They are considered to be the 'standard brand' for e-gamers. Their products are known to have unique designs that are bold and 'futuristic'.",
+                    "Dell is a company that specializes in computer hardware, like HP. The company is well known for their computer processors. In addition, the monitors produced by Dell are considered to be 'one of the best' in the industry.",
+                    "Linux is an open source OS. In addition to Windows and Mac OS, Linux is a well-known OS that is used by many coders. The simplicity of the OS is well-liked.",
+                  "IPhone XR starts at $749 for 64GB of storage, but can cost up to $899 for 256GB of storage.",
+                "IPhone XS Max starts at $1,099 for 64GB of storage, but can cost up to $1,449 for 512GB of storage.",
+                "IPhone XS starts at $999 for 64GB of storage, but can cost up to $1,349 for 512GB of storage.",
+                "IPhone X starts at $999 for 64GB of storage, but can cost up to $1,149 for 256GB of storage.",
+                "IPhone 8 Plus starts at $699 for 64GB of storage, but can cost up to $849 for 256GB of storage.",
+                "IPhone 8 starts at $699 for 64GB of storage, but can cost up to $749 for 256GB of storage."];
+var url;
+console.log(images);
+for(var i=0; i < images.length; i++){
+  images[i];
+  captions[i];
+  descriptions[i];
+}

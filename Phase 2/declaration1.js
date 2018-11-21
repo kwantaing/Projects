4ad621b711052e8a317ab88b5d815f9b14135f4f
kwantaing <45217862+kwantaing@users.noreply.github.com>
2018-11-20 17:17:57 -0800

    Add files via upload

---
+++ Phase 2/declaration1.js
@@ -0,0 +1,15 @@
+var images = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg"];
+var captions = ["Apple","Microsoft","Hewlett Packard","Alienware","Dell","Linux"];
+var descriptions = ["Apple is a company based in Cupertino, California. Well known for its iPhone product range. Also produces computers and that enrich our society today.",
+                    "Microsoft is a company that was founded by Bill Gates. The company revolutionized Microsoft Windows OS. Without Microsoft, mobile computing would not the same as today.",
+                    "HP is a company that specializes in a variety of computer hardware products. They are well known in the business industry for their monitors. In addition, they are used by professional networking-industries.",
+                    "Alienware is a company that is well known for their gaming computers. They are considered to be the 'standard brand' for e-gamers. Their products are known to have unique designs that are bold and 'futuristic'.",
+                    "Dell is a company that specializes in computer hardware, like HP. The company is well known for their computer processors. In addition, the monitors produced by Dell are considered to be 'one of the best' in the industry.",
+                    "Linux is an open source OS. In addition to Windows and Mac OS, Linux is a well-known OS that is used by many coders. The simplicity of the OS is well-liked."];
+var url;
+console.log(images);
+for(var i=0; i < images.length; i++){
+  images[i];
+  captions[i];
+  descriptions[i];
+}
